5f8b20db2a658ba2c408c6af05d8688202de71bc
elena-ioana-enache <elena.ioana.enache@gmail.com>
2021-10-25 14:09:47 +0300

    Add mui and style TaskWidget

--- task-app/src/components/TaskWidget/TaskWidget.tsx
+++ task-app/src/components/TaskWidget/TaskWidget.tsx
@@ -1,16 +1,60 @@
 import { Task } from "../../interfaces/Tasks";
+import { TextField } from "@mui/material";
+import { DatePicker, LocalizationProvider } from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import { Box } from "@mui/system";
+import { useState } from "react";
+import styles from './TaskWidget.module.scss';
 
 interface Props {
   task: Task;
 }
 const TaskWidget = ({ task }: Props) => {
-  const { name, description, date, status } = task;
+  const { name, description, status } = task;
+  const [date, setDate] = useState<string | null>(task.date);
   return (
-    <div>
-      <span>{name}</span>
-      <span>{description}</span>
-      <span>{date}</span>
-      <span>{status}</span>
-    </div>)
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      m: 3,
+      p:2,
+      border: 1,
+      borderColor: '#F0F0F0',
+      borderRadius: 2,
+    }}>
+      <Box sx={{ mb: 2 }}>
+        <TextField
+          label="Name"
+          variant="outlined"
+          value={name}
+          fullWidth
+        />
+      </Box>
+      <Box sx={{ mb: 2 }}>
+        <TextField
+          label="Description"
+          multiline
+          rows={3}
+          variant="outlined"
+          value={description}
+          fullWidth
+        />
+      </Box>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DatePicker
+          label="Date"
+          value={date}
+          inputFormat="yyyy-MM-dd"
+          onChange={(newValue) => {
+            setDate(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+      <Box sx={{ mt: 2 }}>
+        <span className={styles.status}>{status}</span>
+      </Box>
+    </Box>)
 }
 export default TaskWidget;
